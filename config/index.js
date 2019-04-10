1d3e76aa223b921b0af75fff663365e5a62c089f
smith0811 <anywhere0811@outlook.com>
2019-04-10 14:23:47 +0800

    first commit

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost:27017/diplomski',
+    // mongoURI: 'mongodb://localhost:27017/diplomski',
+    mongoURI: 'mongodb://smith0811:Testtest_01@cluster0-shard-00-00-dvaqf.mongodb.net:27017,cluster0-shard-00-01-dvaqf.mongodb.net:27017,cluster0-shard-00-02-dvaqf.mongodb.net:27017/diplomski?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
     secretOrKey: 'secret'
 };
